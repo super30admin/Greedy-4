23f33de965e976a7776b25f2c45d016344d0824f
arcsoftech <arihant_Chhajed@yahoo.in>
2020-06-15 21:55:23 -0500

    Updated string formation

--- stringFormation.java
+++ stringFormation.java
@@ -1,30 +1,24 @@
 // Time Complexity :O(mn) where m is the length of source and n is the length of the target.
-// Space Complexity :O(m);
+// Space Complexity :O(1);
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
 
 
 // Your code here along with comments explaining your approach
 
-public class Solution {
+class Solution {
     public int shortestWay(String source, String target) {
-        HashSet<Character> set = new HashSet<>();
-        for(Character c:source.toCharArray())
-        {
-            set.add(c);
-        }
-        for(Character c: target.toCharArray())
-        {
-            if(!set.contains(c)) return -1;
-        }
+
         int i = 0; int j = target.length();
         int count = 0;
         while(i<j)
         {
+            int curr = i;
             for(int k =0;k <source.length() && i < j;k++)
             {
                 if(source.charAt(k)==target.charAt(i)) i++;
             }
+            if(curr == i) return -1;
             count++;
         }
         return  count;
