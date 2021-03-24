ad2e82dc18ce229416a2ba993c776e8257861361
Anshul Atriek <atriekak@gmail.com>
2021-03-24 02:02:48 -0400

    Added another approach to Prob 1

--- Minimum Domino Rotations For Equal Row.py
+++ Minimum Domino Rotations For Equal Row.py
@@ -1,11 +1,35 @@
 class Solution:
+    #Solution 1
     def minDominoRotations(self, A: List[int], B: List[int]) -> int:
         #Approach: Greedy
         #Time Complexity: O(n)
         #Space Complexity: O(1)
         #where, n is the length of lists A and B
         
-        a, b = A[0], B[0]
+        result = self.check(A, B, A[0])
+        if result != -1:
+            return result
+        return self.check(A, B, B[0])
+        
+    def check(self, A, B, target):
+        aRot = bRot = 0
+        for i in range(len(A)):
+            if A[i] != target and B[i] != target:
+                return -1
+            elif A[i] != target:
+                aRot += 1
+            elif B[i] != target:
+                bRot += 1
+                
+        return min(aRot, bRot)
+    
+    #Solution 2
+    """
+    def minDominoRotations(self, A: List[int], B: List[int]) -> int:
+        #Approach: Greedy
+        #Time Complexity: O(n)
+        #Space Complexity: O(1)
+        #where, n is the length of lists A and B
         
         aCount = [0 for _ in range(6)]
         bCount = [0 for _ in range(6)]
@@ -18,7 +42,7 @@ class Solution:
             if A[i] != B[i]:
                 orCount[B[i] - 1] += 1
             
-            if max(orCount[a - 1], orCount[b - 1]) < i + 1:
+            if max(orCount) < i + 1:
                 return -1
         
         result = inf
@@ -26,3 +50,4 @@ class Solution:
             result = min(result, len(A) - aCount[i], len(B) - bCount[i])
                 
         return -1 if result == inf else result
+    """
